748305c71d81823d8c79656059f75601724873e2
Domsabianca16 <biancadomsa04@gmail.com>
2024-08-07 13:30:22 +0300

    [Course8] Homework 1

--- src/main/java/course7/TemaSecondTry/Homework7Runner.java
+++ src/main/java/course7/TemaSecondTry/Homework7Runner.java
@@ -4,6 +4,10 @@ import java.util.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.UUID.randomUUID;
 
 public class Homework7Runner  {
     public static void main(String[] args) {
@@ -16,9 +20,9 @@ public class Homework7Runner  {
 
         System.out.println("\nCustom comparator 1 : course number and difficulty level");
 
-        FirstComparator a1 = new FirstComparator(UUID.randomUUID(),1,"C1","HOMEWORK1",DifficultyLevel.EASY);
-        FirstComparator a2 = new FirstComparator(UUID.randomUUID(),2,"C2","HOMEWORK2",DifficultyLevel.MEDIUM);
-        FirstComparator a3 = new FirstComparator(UUID.randomUUID(),0,"C1","HOMEWORK1",DifficultyLevel.MEDIUM);
+        FirstComparator a1 = new FirstComparator(randomUUID(),1,"C1","HOMEWORK1",DifficultyLevel.EASY);
+        FirstComparator a2 = new FirstComparator(randomUUID(),2,"C2","HOMEWORK2",DifficultyLevel.MEDIUM);
+        FirstComparator a3 = new FirstComparator(randomUUID(),0,"C1","HOMEWORK1",DifficultyLevel.MEDIUM);
         List<FirstComparator> assignments = new ArrayList<>();
         assignments.add(a1);
         assignments.add(a2);
@@ -29,9 +33,9 @@ public class Homework7Runner  {
 
         System.out.println("\nCustom comparator 2 : course number and assignment title");
 
-        SecondComparator a4 = new SecondComparator(UUID.randomUUID(),4,"C4","HOMEWORK4",DifficultyLevel.EASY);
-        SecondComparator a5 = new SecondComparator(UUID.randomUUID(),6,"C6","HOMEWORK6",DifficultyLevel.MEDIUM);
-        SecondComparator a6 = new SecondComparator(UUID.randomUUID(),3,"C3","HOMEWORK3",DifficultyLevel.EASY);
+        SecondComparator a4 = new SecondComparator(randomUUID(),4,"C4","HOMEWORK4",DifficultyLevel.EASY);
+        SecondComparator a5 = new SecondComparator(randomUUID(),6,"C6","HOMEWORK6",DifficultyLevel.MEDIUM);
+        SecondComparator a6 = new SecondComparator(randomUUID(),3,"C3","HOMEWORK3",DifficultyLevel.EASY);
         List<SecondComparator> assignments2 = new ArrayList<>();
         assignments2.add(a4);
         assignments2.add(a6);
@@ -46,6 +50,76 @@ public class Homework7Runner  {
         map.put("C",a3);
         System.out.println(map);
         System.out.println(map.get("A"));
+
+        // HOMEWORK 1 COURSE 8:
+
+        System.out.println("\n Homework 8: ");
+        List<Assignment> assignments3 = new ArrayList<>();
+        assignments3.add(new Assignment(randomUUID(),8,"C8","HOMEWORK8",DifficultyLevel.EASY));
+        assignments3.add(new Assignment(randomUUID(),9,"C9","HOMEWORK9",DifficultyLevel.MEDIUM));
+        assignments3.add(new Assignment(randomUUID(),10,"C10","HOMEWORK10",DifficultyLevel.HARD));
+
+        // The elements of the list :
+
+        for(Assignment assignment : assignments3) {
+            System.out.println(assignment);
+        }
+        //1.1 Unique Id s:
+
+        final List<String> uniqueIds = assignments3.stream()
+                .map(assignment -> assignment.getId().toString())
+                .collect(Collectors.toList());
+
+        System.out.println("\nUnique Id's List:  " + uniqueIds);
+
+        //1.2 Filtered to have only Difficulty Levels easy or medium :
+
+        List<Assignment> filteredListByDiff = assignments3.stream()
+               .filter(assignment -> assignment.getLevel().equals(DifficultyLevel.EASY)
+                       ||assignment.getLevel().equals(DifficultyLevel.MEDIUM))
+               .collect(Collectors.toList());
+
+       System.out.println("\nList sorted by difficulty levels easy and medium: ");
+
+       for(Assignment assignment : filteredListByDiff) {
+           System.out.println(assignment);
+       }
+
+       //1.3
+
+        System.out.println("\nEach assignment printed : ");
+        assignments3.stream().peek(System.out::println)
+                .forEach(assignment -> {});
+
+        //1.4  HashMap:
+
+        Map<UUID, Assignment> hashMap =  assignments3.stream()
+                .collect(Collectors
+                        .toMap(Assignment::getId, Function.identity()));
+
+        hashMap.forEach((uuid, assignment) -> {
+            System.out.println("The id : " + uuid + " has the assignment : " + assignment);
+                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
